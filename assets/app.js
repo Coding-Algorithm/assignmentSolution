bd2446783d3dc8ce64e44318f9e317f9127e24bc
Algorithm <taofeekibrahimope@gmail.com>
2022-01-28 01:28:31 +0100

    Assignment Solution

---
+++ assets/app.js
@@ -0,0 +1,50 @@
+let courseDetails = document.querySelectorAll('.course .courseDetails');
+courseDetails = [...courseDetails]
+
+console.log(courseDetails)
+
+let actives = []
+
+function toggle(e) {
+    course = e.target
+    
+    if(course.classList.contains('courseTitle') || course.classList.contains('moreLess')){
+        course = course.parentNode
+    }
+ 
+    console.log(course)
+    
+    moreLess = e.target.children[0]
+        
+    if(!course.classList.contains('active')){
+        moreLess.innerHTML = '-'
+        console.log(actives.length)
+        if(actives.length > 0){
+            console.log(actives[0])
+            console.log('insidde if')
+            moreLess1 = actives[0].children[0]
+            actives[0].classList.toggle('active')
+            actives[0].style.backgroundColor = "white"
+            moreLess1.innerHTML = '+'
+            console.log(actives[0])
+            actives.pop()            
+        }
+        course.classList.toggle('active')
+        course.style.backgroundColor = "grey"
+        actives.push(course)
+    }else{
+        moreLess.innerHTML = '+'
+        actives[0].style.backgroundColor = "white"
+        console.log(course)
+        course.classList.toggle('active')
+        actives.pop()            
+        console.log(course)
+        // actives.pop(actives[0])
+    }
+}
+
+
+courseDetails.forEach(courseDetail => {
+    courseDetail.addEventListener('click', toggle)
+    
+})
